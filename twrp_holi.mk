8563059f444fd0311b1b7df7b3b5cd1533d19311
IamAzmathullaShaikh <107968141+IamAzmathullaShaikh@users.noreply.github.com>
2024-04-05 22:31:17 +0530

    Update Omni Syntaxes with twrp and Device Infos

---
+++ twrp_holi.mk
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/qualcomm/holi
+
+# Release name
+PRODUCT_RELEASE_NAME := holi
+
+# Inherit from holi device
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := holi
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := holi
+PRODUCT_NAME := twrp_holi
+PRODUCT_BRAND := qualcomm
+PRODUCT_MODEL := qualcomm
+PRODUCT_MANUFACTURER := qualcomm
