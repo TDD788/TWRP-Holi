99ad54a502d3fbd22c0ba2bdfeb52a33c9ebb024
IamAzmathullaShaikh <107968141+IamAzmathullaShaikh@users.noreply.github.com>
2024-04-05 23:38:37 +0530

    holi

--- Android.mk
+++ Android.mk
@@ -1,12 +1,5 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),holi)
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
+ifneq ($(filter holi,$(TARGET_DEVICE)),)
+include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
